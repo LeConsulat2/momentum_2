6d257a4bb958a6d3bb6eecc5498d6ca7113d7537
LeConsulat2 <wooine2@gmail.com>
2024-05-04 17:28:14 +1200

    Lets go

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting =document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function onLoginSubmit() {    
-    const username =loginInput.value;
-    console.log(username);
-}
-
-function handleLinkClick() {
+function onLoginSubmit(event) {    
     event.preventDefault();
-    alert("clicked");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+
+    // greeting.innerText ="Hello " + username;
+    // greeting.innerText =`Hello ${username}`;
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}   
